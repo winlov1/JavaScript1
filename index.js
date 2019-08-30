7406d8b4ece24edba88f0ed366291202d1c7b28a
winslet <50847773+winlov1@users.noreply.github.com>
2019-08-30 10:15:16 -0700

    Add my js file

---
+++ index.js
@@ -0,0 +1,98 @@
+
+//my homework
+
+let salute = {english:'Hello', Amaric:'Salam', arabic: 'Marhabaan', turkish:'Marhaba', greek:'Yia-sas'};
+console.log(salute);
+
+
+
+
+console.log("I'm awesome");
+
+
+
+
+let x;
+console.log('variable x will be: undifined');
+console.log(x);
+x = 200;
+console.log('variable x will be: 200');
+console.log(x);
+
+
+
+let y = "winny";
+console.log('the string will be: winny');
+console.log(y);
+
+
+
+let z = 7.25;
+console.log(z);
+ 
+let a = z.toFixed(0);
+console.log(a)
+let h = a<z;
+if (a>z){
+console.log(a)
+}
+else {
+console.log(z)
+}
+
+
+
+let makeup = [];
+console.log('squre brackets')
+console.log(makeup)
+let animals = ['monkey', 'giraffe', 'lion', 'peacock']
+console.log(animals)
+animals.push('big pig')
+console.log(animals)
+
+
+
+var string = "this is a test";
+console.log(string)
+console.log(string.length);
+
+
+let num1 = 45;
+let fruit = "love";
+const num2 = null;
+let ask = false;
+console.log(num1);
+console.log(fruit);
+console.log(num2);
+console.log(ask);
+console.log("types = Number String Null Boolean");
+console.log(typeof num1);
+console.log(typeof fruit);
+console.log(typeof num2);
+console.log(typeof ask);
+if (num1 === fruit) {
+    console.log('Same Type')
+}
+else{
+    console.log('not same type')
+}
+
+
+var d = 7 % 3; 
+console.log(d);
+console.log('% is reminder') 
+console.log('% = 1') 
+
+
+
+let mixed = ["solomon", "winslet", 45, "Ethiopia", 444 ];
+console.log(mixed);
+
+const number1 = 6/0;
+const number2 = 10/0;
+if (number1 === number2){
+    console.log("same number")
+}
+else{
+    console.log("Not the same number")
+}
